40176d03e1543990abf8016751ae4905e4af3ede
Tianjian Wang <wangtjwork@gmail.com>
2018-04-28 21:15:44 -0500

    initialize react native project

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Wan shang da pao ma</Text>
+        <Text>Dao di da bu da</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
