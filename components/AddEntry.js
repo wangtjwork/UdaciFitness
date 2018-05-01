350679c728b876cdb87a05a72dad3cac08e2abe5
Tianjian Wang <wangtjwork@gmail.com>
2018-05-01 15:32:02 -0500

    feat: Add already logged UI to AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton';
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -71,8 +73,33 @@ export default class AddEntry extends Component {
     // clear local modification
   }
 
+  reset = () => {
+    const key = timeToString();
+
+
+
+    // Update redux
+    // Navigate to home
+    // save to database
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name="ios-happy-outline"
+            size={100}
+          />
+          <Text>You already logged your information for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
