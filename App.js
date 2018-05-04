798e6f45df5d510771a0f8cdbdba539607fd76c7
Tianjian Wang <wangtjwork@gmail.com>
2018-05-04 15:00:34 -0500

    feat: Add History component

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 import AddEntry from './components/AddEntry';
+import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import entries from './reducers'
+import entries from './reducers';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(entries)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     );
