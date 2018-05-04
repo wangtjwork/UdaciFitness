7c176862a8789b3b00aa525d3e2832edbce6e7fd
Tianjian Wang <wangtjwork@gmail.com>
2018-05-03 23:44:03 -0500

    feat: Add styles to UdaciStepper for layout

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
 
 export default function UdaciSteppers({ max, unit, step, value, onIncrement, onDecrement }) {
   return (
-    <View style={{flexDirection: 'row'}}>
-      <TouchableOpacity onPress={onDecrement}>
-        <FontAwesome name="minus"
-          size={30} color={'black'}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onIncrement}>
-        <FontAwesome name="plus"
-          size={30} color={'black'}
-        />
-      </TouchableOpacity>
+    <View style={[styles.row, { justifyContent: 'space-between' }]}>
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name="minus"
+            size={30} color={'black'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name="plus"
+            size={30} color={'black'}
+          />
+        </TouchableOpacity>
+      </View>
+
       <View>
         <Text>{value}</Text>
         <Text>{unit}</Text>
       </View>
     </View>
+
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  }
+})
