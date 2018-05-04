ab120758b6cc0013d639b1f62f02d58d318d9d81
Tianjian Wang <wangtjwork@gmail.com>
2018-05-03 23:48:19 -0500

    feat: Add ios style button to UdaciStepper

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { white, purple } from '../utils/colors'
 
 export default function UdaciSteppers({ max, unit, step, value, onIncrement, onDecrement }) {
   return (
     <View style={[styles.row, { justifyContent: 'space-between' }]}>
       <View style={{flexDirection: 'row'}}>
-        <TouchableOpacity onPress={onDecrement}>
+        <TouchableOpacity onPress={onDecrement}
+          style={[styles.iosBtn, {borderTopRightRadius: 0, borderBottomRightRadius: 0}]}>
           <FontAwesome name="minus"
-            size={30} color={'black'}
+            size={30} color={purple}
           />
         </TouchableOpacity>
-        <TouchableOpacity onPress={onIncrement}>
+        <TouchableOpacity onPress={onIncrement}
+          style={[styles.iosBtn, {borderTopLeftRadius: 0, borderBottomLeftRadius: 0}]}>
           <FontAwesome name="plus"
-            size={30} color={'black'}
+            size={30} color={purple}
           />
         </TouchableOpacity>
       </View>
@@ -32,5 +35,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flex: 1,
     alignItems: 'center'
+  },
+  iosBtn: {
+    backgroundColor: white,
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25
   }
 })
