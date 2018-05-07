f1c0d47dfdc58b61f74222519e29ddbc5458781f
Tianjian Wang <wangtjwork@gmail.com>
2018-05-06 23:11:50 -0500

    feat: Add style to renderItem in History

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import { fetchCalendarResults } from '../utils/api';
-import UdaciFitnessCalendar from 'udacifitness-calendar'
+import UdaciFitnessCalendar from 'udacifitness-calendar';
+import { white } from '../utils/colors';
+import DateHeader from './DateHeader';
 
 class History extends Component {
   componentDidMount() {
@@ -23,7 +25,7 @@ class History extends Component {
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today
           ? <Text>{JSON.stringify(today)}</Text>
           : <Text>{JSON.stringify(metrics)}</Text>
@@ -51,6 +53,25 @@ class History extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16: 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  }
+})
 
 const mapStateToProps = (entries) => {
   return {
