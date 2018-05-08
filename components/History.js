242ec25575830e33e141252b7fe19c39b51e7b74
Tianjian Wang <wangtjwork@gmail.com>
2018-05-07 19:49:42 -0500

    feat: Add style to empty data day

--- components/History.js
+++ components/History.js
@@ -27,16 +27,26 @@ class History extends Component {
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
     <View style={styles.item}>
       {today
-          ? <Text>{JSON.stringify(today)}</Text>
-          : <Text>{JSON.stringify(metrics)}</Text>
+          ? <View>
+              <DateHeader date={formattedDate}/>
+              <Text style={styles.noDataText}>
+                {today}
+              </Text>
+            </View>
+          : <TouchableOpacity onPress={() => console.log(pressed)}>
+              <Text>{JSON.stringify(metrics)}</Text>
+            </TouchableOpacity>
       }
     </View>
   )
 
   renderEmptyDate (formattedDate) {
     return (
-      <View>
-        <Text>No data for this day.</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
     )
   }
@@ -70,7 +80,13 @@ const styles = StyleSheet.create({
       width: 0,
       height: 3
     }
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
   }
+
 })
 
 const mapStateToProps = (entries) => {
