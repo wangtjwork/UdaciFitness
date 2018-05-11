be79e12a847d1f2eb5b3b4feee49da6d4bfac29b
Tianjian Wang <wangtjwork@gmail.com>
2018-05-11 15:00:08 -0500

    feat: import packages and add styles

--- components/Live.js
+++ components/Live.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: ''
   }
 
@@ -39,3 +41,28 @@ export default class Live extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText :{
+    color: white,
+    fontSize: 20,
+  }
+})
