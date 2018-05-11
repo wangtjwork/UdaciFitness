3988489608316dfef948bf8f59cac91fe1176eb6
Tianjian Wang <wangtjwork@gmail.com>
2018-05-11 17:56:34 -0500

    feat: Add set notification in helper file

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,11 @@
 // utils/helpers.js
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, AsyncStorage } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { white, red, orange, blue, lightPurp, pink } from './colors'
+import { Notifications, Permissions } from 'expo'
+
+const NOTIFICATION_KEY = 'UdaciFitness:notifications';
 
 const styles = StyleSheet.create({
   iconContainer: {
@@ -161,4 +164,54 @@ export function getDailyReminderValue() {
   return {
     today: "👋 Don't forget to log your data today!"
   }
+}
+
+export function clearLocalNotifications() {
+
+}
+
+function createNotification() {
+  return {
+    title: 'Log your stats!',
+    body: "👋 Don't forget to log your stats for today!",
+    ios: {
+      sound: true
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true
+    }
+  }
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync();
+
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(20);
+              tomorrow.setMinutes(0);
+
+              Notifications.scheduleLocalNotificationsAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true));
+            }
+          })
+      }
+    })
 }
